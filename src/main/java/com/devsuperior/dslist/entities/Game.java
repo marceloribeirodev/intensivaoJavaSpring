e611a588d710c17b6a846b99bf5ed7c3dab711c8
unknown <contatoceloribeiro@gmail.com>
2024-11-12 23:23:08 -0300

    atualização da classe game

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dslist.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_game")
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platform;
